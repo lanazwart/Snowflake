c93043bd2f1c60bcb2b8b3001255382b96935208
Lana Zwart <lanazwart@gmail.com>
2024-10-29 15:23:31 +0000

    Update createDbSchemaStage.sql

--- createDbSchemaStage.sql
+++ createDbSchemaStage.sql
@@ -1,30 +1,31 @@
-//Sample code for creating a database, schema and stage in Snowflake
+/*Sample code for creating a database, schema and stage in Snowflake
 
-//Create some structure DB. Schema, external stage s3://tableautim-snow/yt-datasets/
+Create some structure DB. Schema, external stage s3://tableautim-snow/yt-datasets/ */
 Create or replace database staging ;
 
-Create or replace schema external_stagin;
+Create or replace schema external_staging;
 
 Create or replace stage aws_stage
-url= ‘s3://tableautim-snowflake/linkedin/';
+url=‘s3://tableautim-snowflake/linkedin/';
 
 Desc stage aws_stage;
 
 List @aws_tage;
 
-//Load data into stage from S3 //Preview the data in the stage
+/* Load data into stage from S3 //Preview the data in the stage */
 Select t.$1, t.$2 from @aws_stage t;
 
 Create or replace file format csv_comma type= ‘csv’ field_delimeter= ‘|’ skip_header=1;
 
-//Create a table to load data into
-CREATE TABLE order_data (
-Days_to_Dispatch INTEGER,
-Years_of_sale INTEGER,
-Returned VARCHAR(3),
-Return_notes VARCHAR(500),
-Approver (VARCHAR(100), 
-State_Province (VARCHAR(100),
-Sales_Region varchar(100
+/* Create a table to load data into */ 
+create table ORDER_DATA(
+DAYS_TO_DISPATCH integer(4),
+YEARS_OF_SALE integer(4),
+RETURNED varchar(3),
+RETURN_NOTES varchar(500),
+APPROVER varchar(100), 
+STATE_PROVINCE varchar(100),
+SALES_REGION varchar(100))
+; 
 
 //  ETC - this is the whole table definition 
