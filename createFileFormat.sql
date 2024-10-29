362649a93694ee2182df5291c35fabd04284c09f
Lana Zwart <lanazwart@gmail.com>
2024-10-29 15:33:20 +0000

    Update and rename createFileFormat.SQL to createFileFormat.sql

---
+++ createFileFormat.sql
@@ -0,0 +1,5 @@
+create file format "Myformatfile" 
+type CSV  --can be CSV (structured) or unstructured data types like JSON, Avro, Parquet, Orc and XML
+--File type Modifiers
+FIELD_DELIMETER = '|'
+SKIP_HEADER = 2;
