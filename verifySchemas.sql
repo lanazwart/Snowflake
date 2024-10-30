0fd381106e3ec0bdfcf3d5b9d74bb4e169e978eb
Lana Zwart <lanazwart@gmail.com>
2024-10-30 13:56:16 +0000

    Update verifySchemas.sql

--- verifySchemas.sql
+++ verifySchemas.sql
@@ -3,7 +3,7 @@ select *
 from GARDEN_PLANTS.INFORMATION_SCHEMA.SCHEMATA
 where schema_name in ('FLOWERS','FRUITS','VEGGIES'); 
 
---Check number of schemas
+--Check number of schemas using the INFORMATION_SCHEMA
 select count(*) as schemas_found, '3' as schemas_expected 
 from GARDEN_PLANTS.INFORMATION_SCHEMA.SCHEMATA
 where schema_name in ('FLOWERS','FRUITS','VEGGIES'); 
