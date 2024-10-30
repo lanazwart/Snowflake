2ab746c147b930d9534d48ed389905798c3ccf72
Lana Zwart <lanazwart@gmail.com>
2024-10-30 13:51:42 +0000

    Create createFunctionExample.sql

---
+++ createFunctionExample.sql
@@ -0,0 +1,26 @@
+use role accountadmin;  
+
+create or replace external function util_db.public.grader(
+      step varchar
+    , passed boolean
+    , actual integer
+    , expected integer
+    , description varchar)
+returns variant
+api_integration = dora_api_integration 
+context_headers = (current_timestamp, current_account, current_statement, current_account_name) 
+as 'https://awy6hshxy4.execute-api.us-west-2.amazonaws.com/dev/edu_dora/grader'
+; 
+
+--Check that the function is working
+use role accountadmin;
+use database util_db; 
+use schema public; 
+
+select grader(step, (actual = expected), actual, expected, description) as graded_results from
+(SELECT 
+ 'DORA_IS_WORKING' as step
+ ,(select 123) as actual
+ ,123 as expected
+ ,'Dora is working!' as description
+); 
