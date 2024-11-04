02d6926cd854a794a8174414dfc41784af9d1489
Lana Zwart <lanazwart@gmail.com>
2024-11-04 09:12:54 +0000

    Create copyIntoFromFile.sql

---
+++ copyIntoFromFile.sql
@@ -0,0 +1,4 @@
+copy into vegetable_details_soil_type
+from @util_db.public.my_internal_stage
+files = ( 'VEG_NAME_TO_SOIL_TYPE_PIPE.txt')
+file_format = ( format_name=GARDEN_PLANTS.VEGGIES.PIPECOLSEP_ONEHEADROW );
