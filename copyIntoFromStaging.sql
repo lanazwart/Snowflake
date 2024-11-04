f4f2a84d7aa2aa16deeeeac3eedc85725ef05634
Lana Zwart <lanazwart@gmail.com>
2024-11-04 10:25:19 +0000

    Update copyIntoFromStaging.sql

--- copyIntoFromStaging.sql
+++ copyIntoFromStaging.sql
@@ -1,10 +1,4 @@
---Note that we need a virtual warehouse running before the copy into can be made from a Staging area
-
---Load into table - In Snowflake, position cursor in context of the database / schema you're loading into
-copy into order_data
-from @staging.external_staging.aws_stage
-file_format = (type=‘css’ field_delimeter = ‘|’ skip_header=1 )
-Files = (‘orders.csv’);
-
---Preview first 100 rows of data
-select * from order_data limit 100;
+copy into GARDEN_PLANTS.VEGGIES.LU_SOIL_TYPE 
+from @util_db.public.my_internal_stage
+files = ('LU_SOIL_TYPE.tsv')
+file_format = (format_name=GARDEN_PLANTS.VEGGIES.L9_CHALLENGE_FF);
