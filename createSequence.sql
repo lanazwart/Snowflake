ca05fd0a4b0cde9ab1b7e8beb936b31032fe8753
Lana Zwart <lanazwart@gmail.com>
2024-11-05 16:29:42 +0000

    Create createSequence.sql

---
+++ createSequence.sql
@@ -0,0 +1,7 @@
+create sequence SEQ_AUTHOR_ID
+   start = 1
+   increment = 1
+   ORDER
+   comment = ‘Use this to fill in AUTHOR_ID’;
+   
+--NOTE:  If you do not include the word ORDER, your values will skip by 100 each time.    
