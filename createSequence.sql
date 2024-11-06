76953c6c7c6ccd69bd75cdfd7294c48d64e93c4f
Lana Zwart <lanazwart@gmail.com>
2024-11-06 11:36:21 +0000

    Update createSequence.sql

--- createSequence.sql
+++ createSequence.sql
@@ -5,3 +5,10 @@ create sequence SEQ_AUTHOR_ID
    comment = ‘Use this to fill in AUTHOR_ID’;
    
 --NOTE:  If you do not include the word ORDER, your values will skip by 100 each time.    
+
+//Drop and recreate the counter (sequence) so that it starts at 3 
+create or replace sequence library_card_catalog.public.seq_author_uid
+start = 3 
+increment = 1 
+ORDER
+comment = 'Use this to fill in the AUTHOR_UID every time you add a row';
