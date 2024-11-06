8d1b1f38b28d13df950fd180f460cbb3c69e60af
Lana Zwart <lanazwart@gmail.com>
2024-11-06 11:41:37 +0000

    Create insertWithNextval.sql

---
+++ insertWithNextval.sql
@@ -0,0 +1,7 @@
+--Use the nextval function instead  of putting in the numbers
+insert into author(author_uid,first_name, middle_name, last_name) 
+values
+(seq_author_uid.nextval, 'Laura', 'K','Egendorf')
+,(seq_author_uid.nextval, 'Jan', '','Grover')
+,(seq_author_uid.nextval, 'Jennifer', '','Clapp')
+,(seq_author_uid.nextval, 'Kathleen', '','Petelinsek');
