73f1670ec4bb98a8041f70b3168ceb9d0c810315
Lana Zwart <lanazwart@gmail.com>
2024-11-06 12:23:16 +0000

    Create createFileFormatForJSON.sql

---
+++ createFileFormatForJSON.sql
@@ -0,0 +1,10 @@
+--The default value for each setting is FALSE. To load the file data into separate rows, one of the values will need to be set to TRUE
+
+create file format library_card_catalog.public.json_file_format
+type = 'JSON' 
+compression = 'AUTO' 
+enable_octal = FALSE
+allow_duplicate = TRUE
+strip_outer_array = FALSE
+strip_null_values = FALSE
+ignore_utf8_errors = FALSE; 
