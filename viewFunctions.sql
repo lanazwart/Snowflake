a05580b98c41fd1b1b96c5f0d0bc6655d8d30bf6
Lana Zwart <lanazwart@gmail.com>
2024-11-08 15:32:36 +0000

    Create viewFunctions.sql

---
+++ viewFunctions.sql
@@ -0,0 +1,7 @@
+show user functions in account;
+
+select * 
+from util_db.information_schema.functions
+where function_name = 'GRADER'
+and function_catalog = 'UTIL_DB'
+and function_owner = 'ACCOUNTADMIN';
