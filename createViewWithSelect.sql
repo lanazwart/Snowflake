b8985f0c606a47a0155b6b576f0c2abd4a32330d
Lana Zwart <lanazwart@gmail.com>
2024-11-28 12:55:06 +0000

    Update createViewWithSelect.sql

--- createViewWithSelect.sql
+++ createViewWithSelect.sql
@@ -5,3 +5,10 @@ create view intl_db.public.NATIONS_SAMPLE_PLUS_ISO
   ,region) AS
   <put select statement here>
 ;
+--example
+create view intl_db.public.simple_currency
+( cty_code
+  ,cur_code )
+   AS
+  select COUNTRY_CHAR_CODE, CURRENCY_CHAR_CODE  from  INTL_DB.PUBLIC.COUNTRY_CODE_TO_CURRENCY_CODE 
+;
