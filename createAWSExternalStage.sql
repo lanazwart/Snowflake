c2b091e0d8b9fa413cc513ba0f4cc60d388c662c
Lana Zwart <lanazwart@gmail.com>
2024-12-05 10:43:24 +0000

    Update and rename createAWSExternalStage.sql to refactor2

--- createAWSExternalStage.sql
+++
@@ -1,9 +0,0 @@
---Create a file format and then load each of the 5 Lookup Tables
---You need a file format if you want to load the table
-CREATE FILE FORMAT vin.decode.comma_sep_oneheadrow 
-type = 'CSV' 
-field_delimiter = ',' 
-record_delimiter = '\n' 
-skip_header = 1 
-field_optionally_enclosed_by = '"'  
-trim_space = TRUE;
