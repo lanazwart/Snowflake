f6cf755b17a4284ef1bba86eae152a7c7022e113
Lana Zwart <lanazwart@gmail.com>
2024-12-05 15:15:42 +0000

    Update copyIntoFromAWS.sql

--- copyIntoFromAWS.sql
+++ copyIntoFromAWS.sql
@@ -21,3 +21,10 @@ copy into intl_db.public.int_stds_org_3166
 from @util_db.public.AWS_S3_BUCKET    --this is the external S3 staging area
 files = ('ISO_Countries_UTF8_pipe.csv')  --this is the file on AWS
 file_format = ( format_name='UTIL_DB.PUBLIC.PIPE_DBLQUOTE_HEADER_CR' ); 
+
+--a special copy into 
+-- With a parsed header, Snowflake can MATCH BY COLUMN NAME during the COPY INTO
+copy into stock.unsold.lotstock
+from @stock.unsold.aws_s3_bucket/Lotties_LotStock_Data.csv
+file_format = (format_name = util_db.public.csv_col_count_diff)
+match_by_column_name='CASE_INSENSITIVE';
