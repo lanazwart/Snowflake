8038921d57cefd884741f042ef477497d433a6b2
Lana Zwart <lanazwart@gmail.com>
2025-01-02 17:08:23 +0000

    Create geoFunctionToAcceptGeographicData.sql

---
+++ geoFunctionToAcceptGeographicData.sql
@@ -0,0 +1,11 @@
+---when your input is stored in a geographic object
+CREATE OR REPLACE FUNCTION distance_to_mc(lng_and_lat GEOGRAPHY)
+  RETURNS FLOAT
+  AS
+  $$
+   st_distance(
+        st_makepoint('-104.97300245114094','39.76471253574085')
+        ,lng_and_lat
+        )
+  $$
+  ;
