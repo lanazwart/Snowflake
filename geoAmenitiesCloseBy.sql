5922d87aac8483f97c242921960429d2b7c485fc
Lana Zwart <lanazwart@gmail.com>
2025-01-02 16:50:16 +0000

    Create geoAmenitiesCloseBy.sql

---
+++ geoAmenitiesCloseBy.sql
@@ -0,0 +1,10 @@
+--- using a marketplace data source (Openstreetmap_denver) containing geo data
+select * 
+from OPENSTREETMAP_DENVER.DENVER.V_OSM_DEN_AMENITY_SUSTENANCE
+where 
+    ((amenity in ('fast_food','cafe','restaurant','juice_bar'))
+    and 
+    (name ilike '%jamba%' or name ilike '%juice%'
+     or name ilike '%superfruit%'))
+ or 
+    (cuisine like '%smoothie%' or cuisine like '%juice%');
