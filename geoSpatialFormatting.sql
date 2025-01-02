2748d0eeaf8e4c3cbf20288f26a8f0f373455731
Lana Zwart <lanazwart@gmail.com>
2025-01-02 15:55:09 +0000

    Update and rename geoSpatialFormatting.sq; to geoSpatialFormatting.sql

---
+++ geoSpatialFormatting.sql
@@ -0,0 +1,30 @@
+--Nicely formatted trail data (from WKT playground data source)
+select 
+ $1:sequence_1 as point_id,
+ $1:trail_name::varchar as trail_name,
+ $1:latitude::number(11,8) as lng, --remember we did a gut check on this data
+ $1:longitude::number(11,8) as lat
+from @trails_parquet
+(file_format => ff_parquet)
+order by point_id;
+
+--Add more GeoSpatial Calculations to get more GeoSpecial Information! 
+create view trails_and_boundaries as
+select feature_name
+, to_geography(geometry) as my_linestring
+, st_xmin(my_linestring) as min_eastwest
+, st_xmax(my_linestring) as max_eastwest
+, st_ymin(my_linestring) as min_northsouth
+, st_ymax(my_linestring) as max_northsouth
+, trail_length
+from DENVER_AREA_TRAILS
+union all
+select feature_name
+, to_geography(geometry) as my_linestring
+, st_xmin(my_linestring) as min_eastwest
+, st_xmax(my_linestring) as max_eastwest
+, st_ymin(my_linestring) as min_northsouth
+, st_ymax(my_linestring) as max_northsouth
+, trail_length
+from DENVER_AREA_TRAILS_2;
+
