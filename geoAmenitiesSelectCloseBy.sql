e6b1a632d8f6e5c4bc71011abe486989114a1de1
Lana Zwart <lanazwart@gmail.com>
2025-01-02 17:01:56 +0000

    Update and rename geoAmenitiesCloseBy.sql to geoAmenitiesSelectCloseBy.sql

--- geoAmenitiesSelectCloseBy.sql
+++ geoAmenitiesSelectCloseBy.sql
@@ -8,3 +8,15 @@ where
      or name ilike '%superfruit%'))
  or 
     (cuisine like '%smoothie%' or cuisine like '%juice%');
+
+--To see which competition is closest to Melanie's (example business used during trial)
+SELECT
+ name
+ ,cuisine
+ , ST_DISTANCE(
+    st_makepoint('-104.97300245114094','39.76471253574085')
+    , coordinates
+  ) AS distance_to_melanies
+ ,*
+FROM  competition
+ORDER by distance_to_melanies;
