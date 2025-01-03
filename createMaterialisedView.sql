feb02062b99cbf579ce06db5a5959823035e9984
Lana Zwart <lanazwart@gmail.com>
2025-01-03 12:51:39 +0000

    Create createMaterialisedView.sql

---
+++ createMaterialisedView.sql
@@ -0,0 +1,17 @@
+create secure materialized view MELS_SMOOTHIE_CHALLENGE_DB.TRAILS.SMV_CHERRY_CREEK_TRAIL(
+	POINT_ID,
+	TRAIL_NAME,
+	LNG,
+	LAT,
+	COORD_PAIR,
+    DISTANCE_TO_MELANIES
+) as
+select 
+ value:sequence_1 as point_id,
+ value:trail_name::varchar as trail_name,
+ value:latitude::number(11,8) as lng,
+ value:longitude::number(11,8) as lat,
+ lng||' '||lat as coord_pair,
+ locations.distance_to_mc(lng,lat) as distance_to_melanies
+from t_cherry_creek_trail;
+ 
