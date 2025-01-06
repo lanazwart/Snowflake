f8bfd644d0a321d4d8b0e948674db68109229a4a
Lana Zwart <lanazwart@gmail.com>
2025-01-06 11:24:44 +0000

    Create createIcebergTable.sql

---
+++ createIcebergTable.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE EXTERNAL VOLUME iceberg_external_volume
+   STORAGE_LOCATIONS =
+      (
+         (
+            NAME = 'iceberg-s3-us-west-2'
+            STORAGE_PROVIDER = 'S3'
+            STORAGE_BASE_URL = 's3://uni-dlkw-iceberg'
+            STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::321463406630:role/dlkw_iceberg_role'
+            STORAGE_AWS_EXTERNAL_ID = 'dlkw_iceberg_id'
+         )
+      );
