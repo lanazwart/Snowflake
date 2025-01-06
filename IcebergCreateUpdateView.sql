28676a3899cc851afa5ed2d127d0afcc78eb3cfc
Lana Zwart <lanazwart@gmail.com>
2025-01-06 12:00:54 +0000

    Create IcebergCreateUpdateView.sql

---
+++ IcebergCreateUpdateView.sql
@@ -0,0 +1,23 @@
+set table_name = 'CCT_'||current_account();
+
+create iceberg table identifier($table_name) (
+    point_id number(10,0)
+    , trail_name string
+    , coord_pair string
+    , distance_to_melanies decimal(20,10)
+    , user_name string
+)
+  BASE_LOCATION = $table_name
+  AS SELECT top 100
+    point_id
+    , trail_name
+    , coord_pair
+    , distance_to_melanies
+    , current_user()
+  FROM MELS_SMOOTHIE_CHALLENGE_DB.TRAILS.SMV_CHERRY_CREEK_TRAIL;
+
+select * from identifier($table_name); 
+
+update identifier($table_name)
+set user_name = 'I am amazing!!'
+where point_id = 1;
