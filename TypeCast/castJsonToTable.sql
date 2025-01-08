20489125ecef4ca09b377f9ce579028c4eac36b1
Lana Zwart <lanazwart@gmail.com>
2025-01-08 16:48:16 +0000

    Update castJsonToTable.sql

--- TypeCast/castJsonToTable.sql
+++ TypeCast/castJsonToTable.sql
@@ -7,3 +7,14 @@ RAWLOG:agent::text as AGENT
 , RAWLOG:user_login::text as USER_LOGIN
 , *
 from game_logs ;
+
+
+--this changes the columns to proper JSON
+create or replace view LOGS as 
+select 
+RAWLOG:agent::text as AGENT
+, TO_TIMESTAMP_NTZ(RAWLOG:datetime_iso8601) AS datetime_iso8601
+, RAWLOG:user_event::text as USER_EVENT
+, RAWLOG:user_login::text as USER_LOGIN
+, TO_JSON(RAWLOG) AS RAW_LOG
+from game_logs ; 
