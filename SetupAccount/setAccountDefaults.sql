036a3134e8ce731fbb94c24eaec51300e151a4d8
Lana Zwart <lanazwart@gmail.com>
2025-01-08 11:09:13 +0000

    Update setAccountDefaults.sql

--- SetupAccount/setAccountDefaults.sql
+++ SetupAccount/setAccountDefaults.sql
@@ -1,3 +1,3 @@
-alter user DEVENG_ADMIN set default_role = 'SYSADMIN';
-alter user DEVENG_ADMIN set default_warehouse = 'COMPUTE_WH';
-alter user DEVENG_ADMIN set default_namespace = 'UTIL_DB.PUBLIC';
+alter user DATAENG_ADMIN set default_role = 'SYSADMIN';
+alter user DATAENG_ADMIN set default_warehouse = 'COMPUTE_WH';
+alter user DATAENG_ADMIN set default_namespace = 'UTIL_DB.PUBLIC';
