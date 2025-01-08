76ede8f0578d61240d03e5df3526c63437668463
Lana Zwart <lanazwart@gmail.com>
2025-01-08 12:11:15 +0000

    Create testAndViewStage.sql

---
+++ Stages/testAndViewStage.sql
@@ -0,0 +1,7 @@
+--list a stage that contains S3 buckets / files
+list @uni_kishore;
+
+--list one of the files in the S3 bucket
+list @uni_kishore/kickoff;
+
+--the md5 column stores an MD5 hash of the contents of the staged data file
