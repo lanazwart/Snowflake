06d02449f986d18aeae0a8f3c618f82a9f2c7237
Lana Zwart <lanazwart@gmail.com>
2025-01-08 11:07:59 +0000

    Create setAccountDefaults.sql

---
+++ SetupAccount/setAccountDefaults.sql
@@ -0,0 +1,3 @@
+alter user DEVENG_ADMIN set default_role = 'SYSADMIN';
+alter user DEVENG_ADMIN set default_warehouse = 'COMPUTE_WH';
+alter user DEVENG_ADMIN set default_namespace = 'UTIL_DB.PUBLIC';
