ca36f98cdf7b5e2b88f9b002fa3e1d8b20b71778
Lana Zwart <lanazwart@gmail.com>
2025-01-08 12:39:03 +0000

    Update copyIntoFromStaging.sql

--- stageFiles/copyIntoFromStaging.sql
+++ stageFiles/copyIntoFromStaging.sql
@@ -1,4 +1,10 @@
+--for when there are many files in the stage, specify the file name
 copy into GARDEN_PLANTS.VEGGIES.LU_SOIL_TYPE 
 from @util_db.public.my_internal_stage
 files = ('LU_SOIL_TYPE.tsv')
 file_format = (format_name=GARDEN_PLANTS.VEGGIES.L9_CHALLENGE_FF);
+
+--for when there is just one file - snowflake will automatically load all files if you don't specify a file name
+copy into ags_game_audience.raw.game_logs
+from @uni_kishore/kickoff
+file_format = (format_name=FF_JSON_LOGS);
