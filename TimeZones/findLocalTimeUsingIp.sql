67374ad777242ca33b23b1e9834dc03293002b54
Lana Zwart <lanazwart@gmail.com>
2025-01-10 10:50:34 +0000

    Update findLocalTimeUsingIp.sql

--- TimeZones/findLocalTimeUsingIp.sql
+++ TimeZones/findLocalTimeUsingIp.sql
@@ -1,6 +1,36 @@
--- reading a marketplace file containing data for IP addresses to find the physical location of where a date in a log comes from
--- used to find where a user is logging in from - won't work 100 as some people use VPNs
+select parse_ip('100.41.16.160', 'inet');
+select parse_ip('100.41.16.160','inet'):host;
+select parse_ip('100.41.16.160','inet'):family;
+select parse_ip('100.41.16.160','inet'):ipv4;
 
+
+--Look up Kishore and Prajina's Time Zone in the IPInfo share using his headset's IP Address with the PARSE_IP function.
+select start_ip, end_ip, start_ip_int, end_ip_int, city, region, country, timezone
+from IPINFO_IP_GEOLOCATION_TRAININGEDUCATION_SAMPLE.DEMO.LOCATION 
+where parse_ip('100.41.16.160', 'inet'):ipv4 --Kishore's Headset's IP Address
+BETWEEN start_ip_int AND end_ip_int;
+--this gives 
+-- a start_IP of 100.41.16.158   
+--  an end_ip of 100.41.16.160   
+-- start_ip_int 1680412830  
+-- end_ip_int 1680412832
+
+--Look up everyone's time zone
+--Join the log and location tables to add time zone to each row using the PARSE_IP function.
+select logs.*
+       , loc.city
+       , loc.region
+       , loc.country
+       , loc.timezone
+from AGS_GAME_AUDIENCE.RAW.LOGS logs
+join IPINFO_IP_GEOLOCATION_TRAININGEDUCATION_SAMPLE.demo.location loc
+where parse_ip(logs.ip_address, 'inet'):ipv4 
+BETWEEN start_ip_int AND end_ip_int;
+--this is not an efficient way of finding timezone and ip info
+
+
+--More efficient way 
+--Use two functions supplied by IPShare to help with an efficient IP Lookup Process!
 SELECT logs.ip_address
 , logs.user_login
 , logs.user_event
@@ -8,10 +38,10 @@ SELECT logs.ip_address
 , city
 , region
 , country
-, timezone -- example Africa/Nairobi or Europe/Warsaw
-, CONVERT_TIMEZONE( 'UTC', timezone, logs.datetime_iso8601) as game_event_ltz  -- taking the datetime fields and converting them into UTC (determined they're stored in UTC through manual tests)
+, timezone 
 from AGS_GAME_AUDIENCE.RAW.LOGS logs
-JOIN IPINFO_IP_GEOLOCATION.demo.location loc 
-ON IPINFO_IP_GEOLOCATION.public.TO_JOIN_KEY(logs.ip_address) = loc.join_key
-AND IPINFO_IP_GEOLOCATION.public.TO_INT(logs.ip_address) 
+JOIN IPINFO_IP_GEOLOCATION_TRAININGEDUCATION_SAMPLE.demo.location loc 
+ON IPINFO_IP_GEOLOCATION_TRAININGEDUCATION_SAMPLE.public.TO_JOIN_KEY(logs.ip_address) = loc.join_key
+AND IPINFO_IP_GEOLOCATION_TRAININGEDUCATION_SAMPLE.public.TO_INT(logs.ip_address) 
 BETWEEN start_ip_int AND end_ip_int;
+
