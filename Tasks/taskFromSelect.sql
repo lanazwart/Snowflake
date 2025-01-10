f265cea211a6f64acc88041f906daf00e68b778e
Lana Zwart <lanazwart@gmail.com>
2025-01-10 15:09:13 +0000

    Update taskFromSelect.sql

--- Tasks/taskFromSelect.sql
+++ Tasks/taskFromSelect.sql
@@ -9,3 +9,12 @@ create task load_logs_enhanced
   -- when <boolean_expr>
   as
     select 'hello';
+
+--Now you should be able to run the task, even if your role is set to SYSADMIN
+execute task AGS_GAME_AUDIENCE.RAW.LOAD_LOGS_ENHANCED;
+
+--the SHOW command might come in handy to look at the task 
+show tasks in account;
+
+--you can also look at any task more in depth using DESCRIBE
+describe task AGS_GAME_AUDIENCE.RAW.LOAD_LOGS_ENHANCED;
