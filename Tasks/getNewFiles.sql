7ecf943aab9aa8ebbc30b449392486f227f9d7f2
Lana Zwart <lanazwart@gmail.com>
2025-01-14 09:07:43 +0000

    Update getNewFiles.sql

--- Tasks/getNewFiles.sql
+++ Tasks/getNewFiles.sql
@@ -4,3 +4,12 @@ create or replace task AGS_GAME_AUDIENCE.RAW.GET_NEW_FILES
 	as copy into  AGS_GAME_AUDIENCE.RAW.PL_GAME_LOGS
     from @AGS_GAME_AUDIENCE.RAW.UNI_KISHORE_PIPELINE 
     file_format = (format_name=AGS_GAME_AUDIENCE.RAW.FF_JSON_LOGS);
+
+--You have to run this grant or you won't be able to test your tasks while in SYSADMIN role
+--this is true even if SYSADMIN owns the task!!
+use role accountadmin;
+grant execute task on account to role SYSADMIN;
+
+use role sysadmin; 
+
+execute task AGS_GAME_AUDIENCE.RAW.GET_NEW_FILES 
