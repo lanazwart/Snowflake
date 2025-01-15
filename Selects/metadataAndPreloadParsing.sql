2a892ff3271bd48ca9fef98158296249bb2007ff
Lana Zwart <lanazwart@gmail.com>
2025-01-15 10:20:48 +0000

    Create metadataAndPreloadParsing.sql

---
+++ Selects/metadataAndPreloadParsing.sql
@@ -0,0 +1,10 @@
+SELECT 
+    METADATA$FILENAME as log_file_name --new metadata column
+  , METADATA$FILE_ROW_NUMBER as log_file_row_id --new metadata column
+  , current_timestamp(0) as load_ltz --new local time of load
+  , get($1,'datetime_iso8601')::timestamp_ntz as DATETIME_ISO8601
+  , get($1,'user_event')::text as USER_EVENT
+  , get($1,'user_login')::text as USER_LOGIN
+  , get($1,'ip_address')::text as IP_ADDRESS    
+  FROM @AGS_GAME_AUDIENCE.RAW.UNI_KISHORE_PIPELINE
+  (file_format => 'ff_json_logs');
